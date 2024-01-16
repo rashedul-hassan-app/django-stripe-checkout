22075468700637fe3e0f366fc6976ad6937f958d
Rashedul Hassan <rashedul@outlook.com>
2024-01-16 19:07:39 +0000

    stripe checkout page appears, no other function implemented yet

--- payments/urls.py
+++ payments/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('config/', views.stripe_config, name='stripe_config'),
+    path('create-checkout-session/', views.create_checkout_session,
+         name='create_checkout_session'),
 ]
--- static/main.js
+++ static/main.js
@@ -2,8 +2,30 @@ console.log("Hello world");
 
 fetch("/config/")
 	.then((result) => {
+		console.log("-- fetch config -- ");
+		console.log(result);
 		return result.json();
 	})
 	.then((data) => {
+		console.log("-- inside data --");
+		console.log(data);
 		const stripe = Stripe(data.publicKey);
+
+		document.querySelector("#submitBtn").addEventListener("click", () => {
+			// Get checkout session ID
+			fetch("/create-checkout-session/")
+				.then((result) => {
+					return result.json();
+				})
+				.then((data) => {
+					console.log(data);
+					// Redirect to Stripe Checkout
+					return stripe.redirectToCheckout({
+						sessionId: data.sessionId,
+					});
+				})
+				.then((result) => {
+					console.log(result);
+				});
+		});
 	});
